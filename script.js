248d5c8b5aed5b3410dcb213fc9693b6b2d931b7
ShivajiRam108-github <ramudus684@gmail.com>
2024-12-15 16:03:01 +0530

    string

---
+++ script.js
@@ -0,0 +1,167 @@
+
+console.log("..............Q -1................")
+
+console.log("Hello World".replace("World", "Universe"));   //Hello Universe
+
+console.log("..............Q -2................")
+
+console.log("foo bar baz".replaceAll("bar", "baz"));  //foo baz baz
+
+console.log("..............Q -3................")
+
+console.log("Hello".charAt(4));                //0
+
+console.log("..............Q -4................")
+
+console.log("Hello World".charAt(-1));             // empty string 
+
+console.log("..............Q -5................")
+
+console.log("Hello".at(-1));                     // o
+
+console.log("..............Q -6................")
+
+console.log("Hello World".length);             // 11
+
+console.log("..............Q -7................")
+
+console.log("JavaScript".slice(4, 10));      // Script
+
+console.log("..............Q -8................")
+
+console.log("JavaScript".slice(-6));           // script
+
+console.log("..............Q -9................")
+
+console.log("JavaScript".substring(4, 10));        // Script
+
+console.log("..............Q -10................")
+
+console.log("Hello".substring(-2));                    // Hello
+
+
+console.log("..............Q -11................")
+
+console.log("Hello".charCodeAt(1));            // 101
+
+console.log("..............Q -12................")
+
+console.log("Hello".toUpperCase());             // HELLO
+
+console.log("..............Q -13................")
+
+console.log("Hello World".split(" "));            //[ 'Hello , "world"]
+
+console.log("..............Q -14................")
+
+console.log("a,b,c".split(","));               //["a","b","c"]
+
+console.log("..............Q -15................")
+
+console.log("abcabcabc".split("a", 2));   //[] "","bc"]
+
+console.log("..............Q -16................")
+
+console.log("JavaScript is fun".indexOf("is"));  // 11
+
+console.log("..............Q -17................")
+
+console.log("JavaScript".slice(-4, -1));          // rip
+
+console.log("..............Q -18................")
+
+console.log("123456789".substr(3, 4));              // 4567
+
+console.log("..............Q -19................")
+
+console.log("JavaScript".startsWith("Java"));       // ture
+
+console.log("..............Q -20................")
+
+console.log("JavaScript".endsWith("Script"));    // ture
+
+
+
+console.log("..............Q -21................")
+
+console.log("  Hello ".trim());                    // Hello
+
+console.log("..............Q -22................")
+
+console.log("abcDEF".toLowerCase());              // abcdef
+
+console.log("..............Q -23................")
+
+console.log("Hello".concat(" ", "World"));           //Hello World
+
+
+console.log("..............Q -24................")
+
+console.log("Hello"[0]);                           // H
+
+console.log("..............Q -25................")
+
+console.log("Hello".charAt(10));                    // empty string
+
+console.log("..............Q -26................")
+
+console.log("Hello World".includes("World"));         // ture
+
+
+console.log("..............Q -27................")
+
+console.log("Hello".repeat(3));                      //HelloHelloHello
+
+console.log("..............Q -28................")
+
+console.log("abc123".search(/[0-9]/));              //3
+
+console.log("..............Q -29................")
+
+console.log("   ".trim().length);                // 0
+
+console.log("..............Q 30................")
+
+console.log("JavaScript".replace("Script", "Fun"));    // JavaFun
+
+console.log("..............Q -31................")
+
+console.log("Hello".padStart(10, "*"));         // *****Hello
+
+
+console.log("..............Q -32................")
+
+console.log("Hello".padEnd(10, "!"));             // Hello !!!!!
+
+console.log("..............Q -33................")
+
+console.log("JavaScript is cool".lastIndexOf("is"));   // 11 
+
+console.log("..............Q -34................")
+
+console.log("Test123".startsWith("T", 1));         // flase
+
+console.log("..............Q -35................")
+
+console.log("Test123".endsWith("3", 6));              // flase
+
+console.log("..............Q -36................")
+
+console.log("JavaScript".slice(4, -2));              // Scri
+
+console.log("..............Q -37................")
+
+console.log("abcdef".split("", 3));                 // "a", "b","c"
+
+console.log("..............Q -38................")
+
+console.log("  spaced out ".trimStart());           // spaced out
+
+console.log("..............Q -39................")
+
+console.log("repeat".repeat(0));                   // empty string
+
+console.log("..............Q -40................")
+
+console.log("12345".indexOf("6"));                     // -1
+
